c451df06b86bd530650565b49426c95ac64cafae
MinsukiZ <jimmyzhang95@yahoo.com>
2019-10-26 14:46:02 -0700

    front end added

---
+++ MyMovieList-frontend/mymovielist-react/src/components/layout/header/index.js
@@ -0,0 +1,8 @@
+import React, { Component, Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+const noAction = e => e.preventDefault();
+
+class Header extends Component {
+  render() {}
+}
