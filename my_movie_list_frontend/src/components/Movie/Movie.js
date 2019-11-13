3235b0480622b9005d6f3eaa4f0cdd1cda9fb86e
MinsukiZ <jimmyzhang95@yahoo.com>
2019-11-12 22:54:36 -0800

    Movie Page done

--- my_movie_list_frontend/src/components/Movie/Movie.js
+++ my_movie_list_frontend/src/components/Movie/Movie.js
@@ -1,10 +1,67 @@
 import React, { Component } from "react";
 import { API_URL, API_KEY } from "../../config";
 import FourColGrid from "../FourColGrid/FourColGrid";
+import Navigation from "../Navigation/Navigation";
+import MovieInfo from "../MovieInfo/MovieInfo";
+import Spinner from "../Spinner/Spinner";
+import "./Movie.css";
 
 class Movie extends Component {
+  state = {
+    movie: null,
+    directors: [],
+    loading: false
+  };
+
+  componentDidMount() {
+    //Fetch the movie
+    const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`;
+    this.fetchItems(endpoint);
+  }
+
+  fetchItems = endpoint => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        console.log(result);
+        if (result.status_code) {
+          this.setState({ loading: false });
+        } else {
+          this.setState({ movie: result }, () => {
+            //.. then fetch actors in the setState callback function
+            const endpoint = `${API_URL}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`;
+            fetch(endpoint)
+              .then(result => result.json())
+              .then(result => {
+                const directors = result.crew.filter(
+                  member => member.job === "Director"
+                );
+
+                this.setState({
+                  actors: result.cast,
+                  directors
+                });
+              });
+          });
+        }
+      });
+  };
+
   render() {
-    return <div>Home</div>;
+    return (
+      <div className="rmdb-movie">
+        {this.state.movie ? (
+          <div>
+            <Navigation movie={this.props.location.movieName} />
+            <MovieInfo
+              movie={this.state.movie}
+              directors={this.state.directors}
+            />
+          </div>
+        ) : null}
+        {this.state.loading ? <Spinner /> : null}
+      </div>
+    );
   }
 }
 
---
+++ my_movie_list_frontend/src/components/MovieInfo/MovieInfo.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from "../../config";
+import FontAwesome from "react-fontawesome";
+import MovieThumb from "../MovieThumb/MovieThumb";
+import "./MovieInfo.css";
+
+const MovieInfo = props => {
+  return (
+    <div
+      className="rmdb-movieinfo"
+      style={{
+        background: props.movie.backdrop_path
+          ? `url('${IMAGE_BASE_URL}${BACKDROP_SIZE}${props.movie.backdrop_path}')`
+          : "#000"
+      }}
+    >
+      <div className="rmdb-movieinfo-content">
+        <div className="rmdb-movieinfo-thumb">
+          <MovieThumb
+            image={
+              props.movie.poster_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${props.movie.poster_path}`
+                : "./images/no_image.jpg"
+            }
+            clickable={false}
+          />
+        </div>
+        <div className="rmdb-movieinfo-text">
+          <h1>{props.movie.title}</h1>
+          <h3>PLOT</h3>
+          <p>{props.movie.overview}</p>
+          {props.directors.length > 1 ? <h3>DIRECTORS</h3> : <h3>DIRECTOR</h3>}
+          {props.directors.map((element, i) => {
+            return (
+              <p key={i} className="rmdb-director">
+                {element.name}
+              </p>
+            );
+          })}
+        </div>
+        <FontAwesome className="fa-film" name="film" size="5x" />
+      </div>
+    </div>
+  );
+};
+export default MovieInfo;
